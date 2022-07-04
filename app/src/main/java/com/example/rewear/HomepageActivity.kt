e3fa69e710195ba35c658fe093d02cceace9c588
Belinda Vela <vela.belinda.k@gmail.com>
2022-07-03 18:48:57 -0700

    Added basic screens and finished log in page

---
+++ app/src/main/java/com/example/rewear/HomepageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rewear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomepageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
