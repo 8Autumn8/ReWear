5cd66a9c98c07c8095ea055ed5556b279d5fbeca
8Autumn8 <66743952+8Autumn8@users.noreply.github.com>
2022-07-10 14:00:03 -0700

    Delete ClothesData.kt

--- app/src/main/java/com/example/rewear/objects/ClothesData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rewear.objects
-
-import android.graphics.Picture
-import java.util.*
-
-data class ClothesData(val ID: Int?, val desc: String?, val picture: Picture?, val username: String?, val dateCreated: Date?, val dateAdded: Date?)
