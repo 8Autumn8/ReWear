b46195fe46dbdb3616a58bdab630543adbdf395e
8Autumn8 <66743952+8Autumn8@users.noreply.github.com>
2022-07-10 14:17:13 -0700

    Update ClothesData.kt

--- app/src/main/java/com/example/rewear/objects/ClothesData.kt
+++ app/src/main/java/com/example/rewear/objects/ClothesData.kt
@@ -1,1 +1,11 @@
 
+package com.example.rewear.objects
+
+import android.graphics.Picture
+import java.util.*
+
+data class ClothesData(val clothes_id: Int?,
+                       val user_id: Int?,
+                       val picture: Picture?,
+                       val clothes_desc: String?,
+                       val date_created: Date?)
